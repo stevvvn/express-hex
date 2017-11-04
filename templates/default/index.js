c59503697210cc894b14a57c1cf8d59bc0b47daf
Steven Snyder <snyder13@bronson.hubzero.org>
2017-11-03 22:36:05 -0400

    new app initializer

---
+++ templates/default/index.js
@@ -0,0 +1,37 @@
+'use strict';
+// @flow
+const SECRET_SIZE = 40;
+
+module.exports = {
+	'descr': 'Stuff most applications need',
+	'files': {
+		'.gitignore': 'Ignore some things you don\'t want to commit',
+		'server.js': 'Application entry point',
+		'views/': 'Home for templates',
+		'views/layouts/': 'Home for layouts common to templates of the same general type',
+		'views/layouts/html.hbs': 'Layout for HTML views',
+		'conf.js': {
+			'descr': 'Commit-able configuration',
+			'impl': (pkg) => {
+				return `'use strict';
+// @flow
+module.exports = {
+	'name': '${pkg.name}'
+};`;
+
+			}
+		},
+		'secrets.js': {
+			'descr': 'Private per-application configuration (session secret, API keys)',
+			'impl': () => {
+				return `'use strict';
+// @flow
+module.exports = {
+	'session': {
+		'secret': '${require('crypto').randomBytes(SECRET_SIZE).toString('base64')}'
+	}
+};`;
+			}
+		}
+	}
+};
