df05a7b39b94df6a7122bb62162473815ef75953
Steven Snyder <snyder13@bronson.hubzero.org>
2017-11-08 15:16:46 -0500

    demonstrate static file service in default template

--- templates/default/index.js
+++ templates/default/index.js
@@ -9,6 +9,7 @@ module.exports = {
 	'files': {
 		'.gitignore': 'Ignore some things you don\'t want to commit',
 		'server.js': 'Application entry point',
+		'migrate.js': 'Migration utility for schema changes',
 		'middleware/': 'Home for middleware',
 		'middleware/index.js': 'Example middleware',
 		'middleware.js': 'Middleware definitions',
@@ -16,6 +17,9 @@ module.exports = {
 		'views/index.hbs': 'Example template',
 		'views/layouts/': 'Home for layouts common to templates of the same general type',
 		'views/layouts/html.hbs': 'Layout for HTML views',
+		'public/': 'Static files',
+		'public/css/': 'Stylesheets',
+		'public/css/site.css': 'Stub stylesheet',
 		'conf.js': {
 			'descr': 'Commit-able configuration',
 			'impl': (pkg: PackageFile): string => {
@@ -36,6 +40,25 @@ module.exports = {
 	'session': {
 		'secret': '${require('crypto').randomBytes(SECRET_SIZE).toString('base64')}'
 	}
+};`;
+			}
+		},
+		'ecosystem.config.js': {
+			'descr': 'App definition to launch in monitored mode with pm2',
+			'impl': (pkg: PackageFile): string => {
+				return `'use strict';
+module.exports = {
+	/**
+	 * Application configuration section
+	 * http://pm2.keymetrics.io/docs/usage/application-declaration/
+	 */
+	'apps': [
+		{
+			'name': '${pkg.name ? pkg.name.toString() : 'unknown'}',
+			'script': 'server.js',
+			'env': { 'DEBUG': 'hex:*' }
+		}
+	]
 };`;
 			}
 		}
