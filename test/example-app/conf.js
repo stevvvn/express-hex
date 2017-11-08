fa2a1cf581f38533cd77a21261fca75eb75e3fb4
Steven Snyder <snyder13@bronson.hubzero.org>
2017-11-07 21:09:54 -0500

    rm test app for now, more structured approach later

--- test/example-app/conf.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-module.exports = {
-	'name': 'Test app',
-	'port': 8345
-};
--- test/example-app/server.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-const hex = require('hex');
-hex.start(__dirname).then(hex.log.info, console.error);
