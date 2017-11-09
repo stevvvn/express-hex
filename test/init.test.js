ea3f17782f2eb69e0a8e7db65b17b3052ac376af
Steven Snyder <snyder13@bronson.hubzero.org>
2017-11-08 19:49:59 -0500

    make the stupid test work more reliably

--- test/init.test.js
+++ test/init.test.js
@@ -6,72 +6,82 @@ const
 	rimraf = require('rimraf'),
 	pkg = JSON.stringify({ 'name': 'init-test-app' }),
 	{ spawn } = require('child_process'),
-	request = require('request-promise')
+	request = require('request-promise'),
+	{ walkSync } = require('fs-walk')
 	;
 
-if (!process.env.SLOW_TESTS) {
-	describe('initializing a new application', () => {
-		test('define SLOW_TESTS to run', () => {});
+jest.timeOut = 30000;
+// setup
+let newInit = false;
+const initialized = new Promise((res) => {
+	// find most-recently modified file in the template to determine whether to refresh (or to mark the vintage of a new initialization)
+	let latest = null;
+	walkSync('./templates/default', (_1, _2, stat) => {
+		const mtime = new Date(stat.mtime).getTime();
+		if (latest === null || stat.mtime > latest) {
+			latest = mtime;
+		}
 	});
-}
-else {
-	// setup
-	const initialized = new Promise((res, rej) => {
-		if (fs.existsSync('./test/init-test-app')) {
-			rimraf('./test/init-test-app', init);
-		}
-		else {
-			init()
-		}
 
-		function init() {
+	if (!fs.existsSync('./test/init-test-app/.vintage') || fs.readFileSync('./test/init-test-app/.vintage') < latest) {
+		console.log('re-initializing test app, this will take some time');
+		newInit = true;
+		rimraf('./test/init-test-app', () => {
 			fs.mkdirSync('./test/init-test-app');
+			fs.writeFileSync('./test/init-test-app/.vintage', latest);
 			fs.writeFileSync('./test/init-test-app/package.json', pkg);
 			res();
-		}
-	});
-
-	describe('initializing a new application', () => {
-		let ran;
-		test('exits without error code', async () => {
-			await initialized;
-			expect.assertions(1);
-
-			ran = new Promise((res) => {
-				spawn('./bin/init', [ 'test/init-test-app', '--template=default' ], { 'stdio': 'ignore' }).on('exit', (code) => {
-					res(code);
-				});
-			});
-			await expect(ran).resolves.toBe(0);
 		});
+	}
+	else {
+		console.log('using existing app in test/init-test-app. delete this path to force testing of init script');
+		res();
+	}
+});
 
-		test('installs at least minimal files', async () => {
-			await ran;
-			const common = [ '.gitignore', 'conf.js', 'server.js', 'migrate.js', 'middleware.js', 'middleware', 'package.json', 'public', 'ecosystem.config.js', 'node_modules/hex' ]
-			expect.assertions(common.length);
-			common.forEach((path) => {
-				expect(fs.existsSync(`./test/init-test-app/${path}`)).toBe(true);
+let ran;
+(newInit ? describe : describe.skip)('initializing a new application', () => {
+	test('exits without error code', async () => {
+		await initialized;
+		expect.assertions(1);
+		ran = new Promise((res) => {
+			spawn('./bin/init', [ `${__dirname}/init-test-app`, '--template=default' ], { 'stdio': 'ignore' }).on('exit', (code) => {
+				res(code);
 			});
 		});
+		await expect(ran).resolves.toBe(0);
+	});
 
-		let server, hex;
-		test('results in a working server', async () => {
-			await ran;
-			hex = require(process.cwd() + '/index');
-			expect.assertions(1);
-			server = hex.start(path.resolve('./test/init-test-app'));
-			await expect(server).resolves.toBeTruthy();
-		});
-
-		test('returns 200 from GET /', async () => {
-			await server;
-			expect.assertions(1);
-			await expect(request.get({
-				'method': 'GET',
-				'uri': 'http://localhost:8000/',
-				'resolveWithFullResponse': true
-			})).resolves.toHaveProperty('statusCode', 200)
-			hex.stop();
+	test('installs at least minimal files', async () => {
+		await ran;
+		const common = [ '.gitignore', 'conf.js', 'server.js', 'migrate.js', 'middleware.js', 'middleware', 'package.json', 'public', 'ecosystem.config.js', 'node_modules/hex' ]
+		expect.assertions(common.length);
+		common.forEach((path) => {
+			expect({ path, 'exists': fs.existsSync(`./test/init-test-app/${path}`) }).toEqual({ path, 'exists': true });
 		});
 	});
-}
+});
+
+describe('server behavior', async () => {
+	if (ran) {
+		await ran;
+	}
+	let server, hex;
+	test('server boots', async () => {
+		expect.assertions(1);
+		hex = require(process.cwd() + '/index');
+		server = hex.start(path.resolve('./test/init-test-app'));
+		await expect(server).resolves.toBeTruthy();
+	});
+
+	test('returns 200 from GET /', async () => {
+		await server;
+		expect.assertions(1);
+		await expect(request.get({
+			'method': 'GET',
+			'uri': 'http://localhost:8000/',
+			'resolveWithFullResponse': true
+		})).resolves.toHaveProperty('statusCode', 200)
+		hex.stop();
+	});
+});
