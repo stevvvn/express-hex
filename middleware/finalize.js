dec4b03f21d3d6e9e73c8ccfa6931e755d788122
Steven Snyder (su) <su-snyder13@labs.nanohub.aws.hubzero.org>
2017-11-14 16:42:01 -0500

    allow generic renderers to be installed by middleware

--- middleware/finalize.js
+++ middleware/finalize.js
@@ -2,7 +2,23 @@
 
 module.exports = ({ app }) => {
 	app.use((req, res, next) => {
-		if (res.content) {
+		if (res.headersSent) {
+			return next();
+		}
+		if (res.renderer) {
+			res.renderer
+				.then(({ type, body }) => {
+					if (type) {
+						res.type(type);
+					}
+					if (body.length) {
+						res.send(body);
+					}
+					res.end();
+				}, (err) => { throw err })
+				.then(() => {}, (err) => { throw err });
+		}
+		else if (res.content) {
 			if (!res.status().statusCode) {
 				res.status(200);
 			}
