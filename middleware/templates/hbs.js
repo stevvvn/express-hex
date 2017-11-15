5e46960566fc0771a873f5643d587fafb21b43be
Steve Snyder <snyder13@purdue.edu>
2017-11-15 17:33:34 -0500

    add support for partials

--- middleware/templates/hbs.js
+++ middleware/templates/hbs.js
@@ -1,10 +1,13 @@
 'use strict';
 // @flow
 
-const hbs = require('express-hbs');
+const hbs = require('express-hbs'), fs = require('fs');
 
 module.exports = ({ conf }) => {
 	return hbs.express4({
-		'layoutsDir': conf.get('paths.launch') + '/views/layouts'
+		'layoutsDir': conf.get('paths.launch') + '/views/layouts',
+		'partialsDir': conf.get('paths.views')
+			.map((path) => { return `${path}/partials`; })
+			.filter(fs.existsSync)
 	});
 };
