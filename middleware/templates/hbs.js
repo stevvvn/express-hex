d8cce7c2142da4665fc5214be46d24dbcedfa25b
Steven Snyder <snyder13@bronson.hubzero.org>
2017-11-17 16:43:51 -0500

    disable partials for now, not implemented properly

--- middleware/templates/hbs.js
+++ middleware/templates/hbs.js
@@ -5,10 +5,14 @@ import type { Conf } from '../../types';
 const hbs = require('express-hbs'), fs = require('fs');
 
 module.exports = ({ conf }: { conf: Conf }) => {
+
+	/// @TODO need to aggregate into one path, having an array of dirs stalls responses indefinitely
+	const partialsDir = conf.get('paths.views')
+			.map((path) => { return `${path}/partials`; })
+			.filter(fs.existsSync);
+
 	return hbs.express4({
 		'layoutsDir': conf.get('paths.launch') + '/views/layouts',
-		'partialsDir': conf.get('paths.views')
-			.map((path) => { return `${path}/partials`; })
-			.filter(fs.existsSync)
+		'partialsDir': ''
 	});
 };
