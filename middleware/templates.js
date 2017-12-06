f9f046ef3a94b9e681c9f482b2e4b378a671d928
Steve Snyder <snyder13@purdue.edu>
2017-12-06 14:22:54 -0500

    fix media attachments when proxied in a folder, provide proxy base to templates

--- middleware/templates.js
+++ middleware/templates.js
@@ -2,11 +2,33 @@
 // @flow
 import type { Conf, App } from '../types';
 
+const fs = require('fs');
+
 module.exports = ({ app, conf }: { app: App, conf: Conf }) => {
-	const engines = conf.get('template.engines', [ 'hbs' ]);
+	const
+		engines = conf.get('template.engines', [ 'hbs' ]),
+		root = conf.get('paths.launch'),
+//		proxyBase = conf.get('http.proxyBase', '/'),
+		siteCss = fs.existsSync(`${root}/public/css/site.css`)
+		;
 
 	engines.forEach((type) => {
 		app.engine(type, require(`${__dirname}/templates/${type}`)({ app, conf }));
+	});
+
+	app.use((req, res, next) => {
+		if (!res.locals._media) {
+			res.locals._media = {};
+			if (siteCss) {
+				res.locals._media['css/site.css'] = true;
+			}
+		}
+		res.attach = (...media) => {
+			media.forEach((file) => {
+				res.locals._media[file] = true;
+			});
+		};
+		next();
 	});
 
 	app.set('view engine', engines[0]);
