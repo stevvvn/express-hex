53d307520a79718b4f4648ec1acd3f15488c6cf0
Steve Snyder <snyder13@purdue.edu>
2018-05-29 11:31:49 -0400

    add body-parser

---
+++ middleware/body-parser.js
@@ -0,0 +1,17 @@
+'use strict';
+const bodyParser = require('body-parser');
+const obj = require('hex-object');
+
+module.exports = ({ app, conf }) => {
+	const settings = obj.wrap({
+		'extended': false,
+		'verify': (req, res, buf, encoding) => {
+			req.rawBody = buf;
+   	}
+	}).augment(conf.get('body-parser', {}));
+
+	app.use(bodyParser.json(settings));
+	app.use(bodyParser.urlencoded(settings));
+	app.use(bodyParser.text(settings));
+	app.use(bodyParser.raw(settings));
+};
