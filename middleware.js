961a891fdb8b1d40c5c3ea344bca85221ba17a6e
Steve Snyder <snyder13@purdue.edu>
2018-11-12 13:23:13 -0500

    add default helmet

--- middleware.js
+++ middleware.js
@@ -5,6 +5,9 @@ import type { MiddlewareDefs } from './types/hex';
 const mw: MiddlewareDefs = {
 	'body-parser': {
 		'description': 'Parse request bodies'
+	},
+	'helmet': {
+		'description': 'Common security headers'
 	}
 };
 module.exports = mw;
